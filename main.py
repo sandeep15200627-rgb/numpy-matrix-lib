91e63c8ca11ec6b5825613ea874e347e06bc85a1
Sandeep <sandeep15200627@gmail.com>
2026-07-05 22:01:07 +0530

    Initial Matrix class implementation

---
+++ main.py
@@ -0,0 +1,4 @@
+from matrix import Matrix
+A=Matrix([[1,2,3],[3,4,5]])
+print(A.data)
+print(type(A.data))
---
+++ matrix.py
@@ -0,0 +1,6 @@
+import numpy as np
+class Matrix:
+    def __init__(self,data):
+      self.data=np.array(data)
+def __add__(self, other):
+       return Matrix(self.data+other.data)
