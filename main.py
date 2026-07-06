e5f709289000f02b83b97d4da15ba48c30c8ab41
Sandeep <sandeep15200627@gmail.com>
2026-07-06 22:19:50 +0530

    Implement matrix subtraction

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from matrix import Matrix
 A=Matrix([[1,2,3],[3,4,5]])
 B=Matrix([[3,4,5],[6,7,8]])
 C=A+B
+D=A-B
 print(C.data)
+print(D.data)
--- matrix.py
+++ matrix.py
@@ -3,4 +3,7 @@ class Matrix:
     def __init__(self,data):
       self.data=np.array(data)
     def __add__(self, other):
-       return Matrix(self.data+other.data)
+       return Matrix(self.data + other.data)
+    def __sub__(self,other):
+       return Matrix(self.data - other.data)
+      
