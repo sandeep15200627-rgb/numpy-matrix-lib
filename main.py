da54bff57d061a294d18b0f02ffc73e6d7f9ca7e
Sandeep <sandeep15200627@gmail.com>
2026-07-06 08:54:14 +0530

    Implement matrix addition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from matrix import Matrix
 A=Matrix([[1,2,3],[3,4,5]])
-print(A.data)
-print(type(A.data))
+B=Matrix([[3,4,5],[6,7,8]])
+C=A+B
+print(C.data)
--- matrix.py
+++ matrix.py
@@ -2,5 +2,5 @@ import numpy as np
 class Matrix:
     def __init__(self,data):
       self.data=np.array(data)
-def __add__(self, other):
+    def __add__(self, other):
        return Matrix(self.data+other.data)
